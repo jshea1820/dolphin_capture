8a81cf12753372da7975839257b85928e951dd09
jshea1820 <jshea1820@gmail.com>
2025-04-18 16:10:24 -0500

    more

--- deploy/userdata.sh
+++ deploy/userdata.sh
@@ -4,5 +4,10 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git
 
+cd /home/ubuntu
+
 # Load the full startup script to the instance
 git clone https://github.com/jshea1820/dolphin_capture.git
+
+# Signal completion
+echo "userdata complete" > signal.txt
